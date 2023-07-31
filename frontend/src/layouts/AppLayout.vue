134bda55f1ff65d08305968017103a676c83c6c6
Katoshi12 <bulat.bulatov.99@bk.ru>
2023-07-31 16:24:49 +0300

    made second section

---
+++ frontend/src/layouts/AppLayout.vue
@@ -0,0 +1,12 @@
+<script setup>
+import AppHeader from "@/layouts/AppHeader.vue";
+</script>
+
+<template>
+  <div class="app_layout">
+    <app-header />
+    <slot />
+  </div>
+</template>
+
+<style scoped lang="scss"></style>
